e1acc68d98883cf10876531ae51962a26cd5f101
Fabio Oliva <contato@fabiooliva.dev>
2025-01-20 21:58:13 -0300

    Initialize project with Fastify setup and environment configuration

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from './app'
+import { env } from './env/index'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 Server listening on http://localhost:3333`)
+  })
